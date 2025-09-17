beab21e0d955a8c6a6927b9e00785c4ee966c2f7
Ohm Patel <ohmap12@gmail.com>
2025-09-17 02:29:41 -0400

    Restructure git

---
+++ Homework 1/Homework01a/Debug/Compiled/subdir_vars.mk
@@ -0,0 +1,16 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+OBJ_SRCS += \
+../Compiled/LCD.obj \
+../Compiled/clocks.obj \
+../Compiled/ports.obj \
+../Compiled/switches.obj \
+../Compiled/system.obj \
+../Compiled/timersB0.obj 
+
+
