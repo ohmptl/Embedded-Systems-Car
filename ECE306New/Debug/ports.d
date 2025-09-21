cf11df59db0ff9b5a8b6ed5e72dfa9847a258b36
Ohm Patel <ohmap12@gmail.com>
2025-09-21 18:32:07 -0400

    CCStudioProject Migration

---
+++ ECE306New/Debug/ports.d
@@ -0,0 +1,33 @@
+# FIXED
+
+ports.obj: ../ports.c
+ports.obj: ../ports.h
+ports.obj: C:/ti/ccs2030/ccs/ccs_base/msp430/include/msp430.h
+ports.obj: C:/ti/ccs2030/ccs/ccs_base/msp430/include/msp430fr2355.h
+ports.obj: C:/ti/ccs2030/ccs/ccs_base/msp430/include/in430.h
+ports.obj: C:/ti/ccs2030/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics.h
+ports.obj: C:/ti/ccs2030/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics_legacy_undefs.h
+ports.obj: C:/ti/ccs2030/ccs/ccs_base/msp430/include/legacy.h
+ports.obj: ../macros.h
+ports.obj: ../ports.h
+
+../ports.c:
+
+../ports.h:
+
+C:/ti/ccs2030/ccs/ccs_base/msp430/include/msp430.h:
+
+C:/ti/ccs2030/ccs/ccs_base/msp430/include/msp430fr2355.h:
+
+C:/ti/ccs2030/ccs/ccs_base/msp430/include/in430.h:
+
+C:/ti/ccs2030/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics.h:
+
+C:/ti/ccs2030/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics_legacy_undefs.h:
+
+C:/ti/ccs2030/ccs/ccs_base/msp430/include/legacy.h:
+
+../macros.h:
+
+../ports.h:
+
